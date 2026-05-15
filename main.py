eb99d9e3d9acd9f828e3b0f443453f0ffb151a41
AlanZeroDev <alangomez1012v2@gmail.com>
2026-05-14 21:39:10 -0300

    crear estructura inicial de FastAPI y modelo Producto

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Base de datos en memoria
+productos = []
+contador_id = 1
+
+class producto(BaseModel):
+    descripcion: str
+    cantidad:int
+    precio:float
+    precio_compra:float
+    precio_venta:float
+    cantidad_vendida:int
+
+
+
