89adfc7fb188199915ed8e1d7daf683416f1526d
AlanZeroDev <alangomez1012v2@gmail.com>
2026-05-30 17:24:41 -0300

    agrego excepciones HTTPException en producto_service

--- backend/app/services/producto_service.py
+++ backend/app/services/producto_service.py
@@ -1,30 +1,49 @@
 from backend.app.database.db import productos, contador_id
 from backend.app.schemas.producto import Producto
+from fastapi import HTTPException
+
 
 def obtener_productos():
     return productos
 
-def agregar_producto(producto:Producto):
+
+def agregar_producto(producto: Producto):
     global contador_id
+
     nuevo = producto.model_dump()
     nuevo["id"] = contador_id
+
     contador_id += 1
+
     productos.append(nuevo)
-    return "producto agregado"
 
-def busqueda_id(id:int):
+    return "Producto agregado"
+
+
+def busqueda_id(id: int):
+
     for producto in productos:
-        if producto["id"] == id :
+        if producto["id"] == id:
             return producto
-    return "No existe el id"
+    raise HTTPException(
+        status_code=404,
+        detail="Id no encontrado"
+    )
 
-def busqueda_descripcion(dato:str):
+
+def busqueda_descripcion(dato: str):
+
     for producto in productos:
         if producto["descripcion"] == dato:
             return producto
-    return "No existe la descripcion"
+    raise HTTPException(
+        status_code=404,
+        detail="Descripcion no encontrada"
+    )
+
 
 def modificar(producto_actualizado: Producto, id: int):
+
     for producto in productos:
         if producto["id"] == id:
             producto["descripcion"] = producto_actualizado.descripcion
@@ -32,35 +51,60 @@ def modificar(producto_actualizado: Producto, id: int):
             producto["precio"] = producto_actualizado.precio
             producto["precio_compra"] = producto_actualizado.precio_compra
             producto["precio_venta"] = producto_actualizado.precio_venta
-            return "producto modificado"
-    return "no existe el producto"
+            return "Producto modificado"
+    raise HTTPException(
+        status_code=404,
+        detail="Id no encontrado"
+    )
 
-def eliminar(id:int):
+
+def eliminar(id: int):
+
     for producto in productos:
         if producto["id"] == id:
             productos.remove(producto)
-            return "producto eliminado"
-    return "id no existe"
+            return "Producto eliminado"
+    raise HTTPException(
+        status_code=404,
+        detail="Id no encontrado"
+    )
 
-def agregar_stock(cantidad:int , id : int):
-    if cantidad > 0:
-        for producto in productos:
-            if producto["id"] == id:
-                stock_actual = producto["cantidad"]
-                producto["cantidad"] = stock_actual + cantidad
-                return producto
-        return "id no encontrado"
-    return "cantidad a agregar debe ser mayor a 0"  
 
-def eliminar_stock(cantidad_eliminar:int , id:int):
-    if cantidad_eliminar <= 0:
-        return "La cantidad a eliminar debe ser mayor a 0"
-    
+def agregar_stock(cantidad: int, id: int):
+
+    if cantidad <= 0:
+        raise HTTPException(
+            status_code=400,
+            detail="La cantidad debe ser mayor a 0"
+        )
     for producto in productos:
-        if producto["id"]==id:
-            if producto["cantidad"] < cantidad_eliminar: 
-                return "Cantidad excede stock"
+        if producto["id"] == id:
+            stock_actual = producto["cantidad"]
+            producto["cantidad"] = stock_actual + cantidad
+            return producto
+    raise HTTPException(
+        status_code=404,
+        detail="Id no encontrado"
+    )
+
+
+def eliminar_stock(cantidad_eliminar: int, id: int):
+    if cantidad_eliminar <= 0:
+        raise HTTPException(
+            status_code=400,
+            detail="La cantidad a eliminar debe ser mayor a 0"
+        )
+    for producto in productos:
+        if producto["id"] == id:
+            if producto["cantidad"] < cantidad_eliminar:
+                raise HTTPException(
+                    status_code=400,
+                    detail="Cantidad excede stock disponible"
+                )
             stock_actual = producto["cantidad"]
             producto["cantidad"] = stock_actual - cantidad_eliminar
             return producto
-        return "id no encontrado"
+    raise HTTPException(
+        status_code=404,
+        detail="Id no encontrado"
+    )
